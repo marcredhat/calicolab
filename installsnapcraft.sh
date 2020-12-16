a923475b8e33e5bb96c6d27a452f9b45a066724b
Marc Chisinevski <marcgcp6@gmail.com>
2020-12-16 12:31:08 -0800

    Update installsnapcraft.sh

--- installsnapcraft.sh
+++ installsnapcraft.sh
@@ -1,5 +1,5 @@
-sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo dnf -y upgrade
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum -y upgrade
 sudo yum -y install snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
